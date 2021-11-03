b7689ead35fee4c2e671346114b50102b4fbfaef
AitorAmbite <51913218+AitorAmbite@users.noreply.github.com>
2021-11-03 09:46:24 +0100

    test await async

--- src/furnitures/furnitures.service.ts
+++ src/furnitures/furnitures.service.ts
@@ -37,8 +37,8 @@ export class FurnituresService {
 
   async findPaginated (page:number,pageSize:number, type?:string) {
     type = type === "all" ? "" : type;
-    let [allFurniture,furnitureCount] = await this.furnitureRepository.findAndCount();
-    const data = await this.furnitureRepository.find({skip:page*pageSize,take:pageSize, where:{type:type}})
+    let [allFurniture,furnitureCount] = this.furnitureRepository.findAndCount();
+    const data = this.furnitureRepository.find({skip:page*pageSize,take:pageSize, where:{type:type}})
     return await {furnitureCount:furnitureCount,data:data}
   }
   async update(id: number, updateFurnitureDto: UpdateFurnitureDto) {
